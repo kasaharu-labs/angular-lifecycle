f0003b8ce238444c6731d43e9a9a04bb30dcabbb
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-19 18:25:59 +0900

    HomeComponent に lifecycle method を実装

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent
+  implements OnInit, OnChanges, OnDestroy, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
   constructor() {}
 
-  ngOnInit() {}
+  ngOnChanges() {
+    console.log('HomeComponent : ngOnChanges -----');
+  }
+
+  ngOnInit() {
+    console.log('HomeComponent : ngOnInit -----');
+  }
+
+  ngDoCheck() {
+    console.log('HomeComponent : ngDoCheck -----');
+  }
+
+  ngAfterContentInit() {
+    console.log('HomeComponent : ngAfterContentInit -----');
+  }
+
+  ngAfterContentChecked() {
+    console.log('HomeComponent : ngAfterContentChecked -----');
+  }
+
+  ngAfterViewInit() {
+    console.log('HomeComponent : ngAfterViewInit -----');
+  }
+
+  ngAfterViewChecked() {
+    console.log('HomeComponent : ngAfterViewChecked -----');
+  }
+
+  ngOnDestroy() {
+    console.log('HomeComponent : ngOnDestroy -----');
+  }
 }
