2964d2a70b00025303dd0526af1808e4e94da2e8
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-19 18:44:36 +0900

    CounterService に BehaviorSubject で状態を管理するロジックを実装

--- src/app/services/counter.service.ts
+++ src/app/services/counter.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CounterService {
-
-  constructor() { }
+  counter$ = new BehaviorSubject<number>(0);
+  constructor() {}
 }
