0f6d4fd9ed30a591322f2863e7cc73d89b3190f4
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-19 18:26:12 +0900

    ChildComponent に lifecycle method を実装

--- src/app/presenters/child/child.component.ts
+++ src/app/presenters/child/child.component.ts
@@ -1,12 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss'],
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent
+  implements OnInit, OnChanges, OnDestroy, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
   constructor() {}
 
-  ngOnInit() {}
+  ngOnChanges() {
+    console.log('ChildComponent : ngOnChanges -----');
+  }
+
+  ngOnInit() {
+    console.log('ChildComponent : ngOnInit -----');
+  }
+
+  ngDoCheck() {
+    console.log('ChildComponent : ngDoCheck -----');
+  }
+
+  ngAfterContentInit() {
+    console.log('ChildComponent : ngAfterContentInit -----');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ChildComponent : ngAfterContentChecked -----');
+  }
+
+  ngAfterViewInit() {
+    console.log('ChildComponent : ngAfterViewInit -----');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ChildComponent : ngAfterViewChecked -----');
+  }
+
+  ngOnDestroy() {
+    console.log('ChildComponent : ngOnDestroy -----');
+  }
 }
