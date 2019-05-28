aff9e983125b8b23ca03cd5475626870cb228239
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-28 10:22:20 +0900

    yarn ng g c presenters/grandchild2 --lint-fix true

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { ChildComponent } from './presenters/child/child.component';
 import { Child2Component } from './presenters/child2/child2.component';
 import { Child3Component } from './presenters/child3/child3.component';
 import { Grandchild1Component } from './presenters/grandchild1/grandchild1.component';
+import { Grandchild2Component } from './presenters/grandchild2/grandchild2.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ChildComponent, Child2Component, Child3Component, Grandchild1Component],
+  declarations: [AppComponent, HomeComponent, ChildComponent, Child2Component, Child3Component, Grandchild1Component, Grandchild2Component],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
