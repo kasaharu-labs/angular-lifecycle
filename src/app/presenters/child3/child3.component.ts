85e63d837f5965675348eda6f0954fb1496d210c
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-28 10:11:53 +0900

    Child3Component にライフサイクルメソッドを実装

--- src/app/presenters/child3/child3.component.ts
+++ src/app/presenters/child3/child3.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-child3',
   templateUrl: './child3.component.html',
-  styleUrls: ['./child3.component.scss']
+  styleUrls: ['./child3.component.scss'],
 })
-export class Child3Component implements OnInit {
+export class Child3Component
+  implements OnInit, OnChanges, OnDestroy, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnChanges() {
+    console.log('Child3Component : ngOnChanges');
   }
 
+  ngOnInit() {
+    console.log('Child3Component : ngOnInit');
+  }
+
+  ngDoCheck() {
+    console.log('Child3Component : ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    console.log('Child3Component : ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    console.log('Child3Component : ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    console.log('Child3Component : ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    console.log('Child3Component : ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    console.log('Child3Component : ngOnDestroy');
+  }
 }
