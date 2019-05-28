d88b308d6e06c3962603bdf5d15d5d9cc90ba1ca
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-28 13:15:19 +0900

    Grandchild1, 2 にライフサイクルメソッドを実装

--- src/app/presenters/grandchild1/grandchild1.component.ts
+++ src/app/presenters/grandchild1/grandchild1.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-grandchild1',
   templateUrl: './grandchild1.component.html',
-  styleUrls: ['./grandchild1.component.scss']
+  styleUrls: ['./grandchild1.component.scss'],
 })
-export class Grandchild1Component implements OnInit {
+export class Grandchild1Component
+  implements OnInit, OnChanges, OnDestroy, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnChanges() {
+    console.log('  GChild1Component : ngOnChanges');
   }
 
+  ngOnInit() {
+    console.log('  GChild1Component : ngOnInit');
+  }
+
+  ngDoCheck() {
+    console.log('  GChild1Component : ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    console.log('  GChild1Component : ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    console.log('  GChild1Component : ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    console.log('  GChild1Component : ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    console.log('  GChild1Component : ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    console.log('  GChild1Component : ngOnDestroy');
+  }
 }
