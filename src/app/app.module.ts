c061a271831c09f5c07e0761de19986d20ab79db
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-28 10:02:11 +0900

    yarn ng g c presenters/child2 --lint-fix true

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ChildComponent } from './presenters/child/child.component';
 import { AnotherChildComponent } from './presenters/another-child/another-child.component';
+import { Child2Component } from './presenters/child2/child2.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ChildComponent, AnotherChildComponent],
+  declarations: [AppComponent, HomeComponent, ChildComponent, AnotherChildComponent, Child2Component],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
