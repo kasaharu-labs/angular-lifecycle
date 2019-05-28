9bffd41e06b01cff5a882dc6f8f438be1552a47c
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-28 10:08:49 +0900

    Child2Component にライフサイクルメソッドを実装

--- src/app/presenters/child2/child2.component.ts
+++ src/app/presenters/child2/child2.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-child2',
   templateUrl: './child2.component.html',
-  styleUrls: ['./child2.component.scss']
+  styleUrls: ['./child2.component.scss'],
 })
-export class Child2Component implements OnInit {
+export class Child2Component
+  implements OnInit, OnChanges, OnDestroy, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnChanges() {
+    console.log('Child2Component : ngOnChanges');
   }
 
+  ngOnInit() {
+    console.log('Child2Component : ngOnInit');
+  }
+
+  ngDoCheck() {
+    console.log('Child2Component : ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    console.log('Child2Component : ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    console.log('Child2Component : ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    console.log('Child2Component : ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    console.log('Child2Component : ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    console.log('Child2Component : ngOnDestroy');
+  }
 }
