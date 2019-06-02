49b2c6ebe3d8c7efc345360437db91fbeddea6cf
Wataru.Kasahara <kasahara1019@gmail.com>
2019-06-02 15:46:27 +0900

    yarn ng g c presenters/child210 --lint-fix true

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { Child200Component } from './presenters/child200/child200.component';
 import { Child300Component } from './presenters/child300/child300.component';
 import { Child310Component } from './presenters/child310/child310.component';
 import { Child320Component } from './presenters/child320/child320.component';
+import { Child210Component } from './presenters/child210/child210.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { Child320Component } from './presenters/child320/child320.component';
     Child300Component,
     Child310Component,
     Child320Component,
+    Child210Component,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
